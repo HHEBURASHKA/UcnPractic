3d24c5f8d29523eab5fef41356d296fc99a7fad6
Андрей Артатомонов <an_drei_artamonov@mail.ru>
2023-12-05 02:24:35 +0500

    Победа

--- UcnPractic/Form1.Designer.cs
+++ UcnPractic/Form1.Designer.cs
@@ -28,57 +28,284 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelС = new System.Windows.Forms.Label();
+            this.labelM = new System.Windows.Forms.Label();
+            this.labelN = new System.Windows.Forms.Label();
+            this.labelEquals = new System.Windows.Forms.Label();
+            this.labelAnswer = new System.Windows.Forms.Label();
+            this.labelP = new System.Windows.Forms.Label();
+            this.labelA = new System.Windows.Forms.Label();
+            this.Solution = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // radioButton1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 52);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
-            // button2
+            // radioButton2
             // 
-            this.button2.Location = new System.Drawing.Point(27, 108);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(23, 68);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
-            // button3
+            // radioButton3
             // 
-            this.button3.Location = new System.Drawing.Point(36, 170);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(23, 103);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(185, 69);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(185, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(185, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(185, 28);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(185, 28);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "m";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(154, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "m";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(154, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "n";
+            // 
+            // labelС
+            // 
+            this.labelС.AutoSize = true;
+            this.labelС.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelС.Location = new System.Drawing.Point(152, 103);
+            this.labelС.Name = "labelС";
+            this.labelС.Size = new System.Drawing.Size(27, 25);
+            this.labelС.TabIndex = 13;
+            this.labelС.Text = "C";
+            // 
+            // labelM
+            // 
+            this.labelM.AutoSize = true;
+            this.labelM.Location = new System.Drawing.Point(164, 94);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(15, 13);
+            this.labelM.TabIndex = 14;
+            this.labelM.Text = "m";
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(166, 128);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(13, 13);
+            this.labelN.TabIndex = 15;
+            this.labelN.Text = "n";
+            // 
+            // labelEquals
+            // 
+            this.labelEquals.AutoSize = true;
+            this.labelEquals.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelEquals.Location = new System.Drawing.Point(181, 111);
+            this.labelEquals.Name = "labelEquals";
+            this.labelEquals.Size = new System.Drawing.Size(18, 20);
+            this.labelEquals.TabIndex = 16;
+            this.labelEquals.Text = "=";
+            // 
+            // labelAnswer
+            // 
+            this.labelAnswer.AutoSize = true;
+            this.labelAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelAnswer.Location = new System.Drawing.Point(205, 111);
+            this.labelAnswer.Name = "labelAnswer";
+            this.labelAnswer.Size = new System.Drawing.Size(20, 24);
+            this.labelAnswer.TabIndex = 17;
+            this.labelAnswer.Text = "?";
+            // 
+            // labelP
+            // 
+            this.labelP.AutoSize = true;
+            this.labelP.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelP.Location = new System.Drawing.Point(153, 107);
+            this.labelP.Name = "labelP";
+            this.labelP.Size = new System.Drawing.Size(26, 25);
+            this.labelP.TabIndex = 18;
+            this.labelP.Text = "P";
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelA.Location = new System.Drawing.Point(153, 106);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(26, 25);
+            this.labelA.TabIndex = 19;
+            this.labelA.Text = "A";
+            // 
+            // Solution
+            // 
+            this.Solution.Location = new System.Drawing.Point(23, 139);
+            this.Solution.Name = "Solution";
+            this.Solution.Size = new System.Drawing.Size(75, 23);
+            this.Solution.TabIndex = 20;
+            this.Solution.Text = "Расчет";
+            this.Solution.UseVisualStyleBackColor = true;
+            this.Solution.Click += new System.EventHandler(this.Solution_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(422, 190);
+            this.Controls.Add(this.Solution);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.labelP);
+            this.Controls.Add(this.labelAnswer);
+            this.Controls.Add(this.labelEquals);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.labelM);
+            this.Controls.Add(this.labelС);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calculater";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelС;
+        private System.Windows.Forms.Label labelM;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Label labelEquals;
+        private System.Windows.Forms.Label labelAnswer;
+        private System.Windows.Forms.Label labelP;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Button Solution;
     }
 }
 
--- UcnPractic/Form1.cs
+++ UcnPractic/Form1.cs
@@ -16,5 +16,244 @@ namespace UcnPractic
         {
             InitializeComponent();
         }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            labelAnswer.Text = "?";
+            if (radioButton1.Checked)
+            {
+                // Показать поля для ввода и вывода, если выбран первый RadioButton
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                label1.Visible = true;
+                label2.Visible = true;
+                labelM.Visible = true;
+                labelN.Visible = true;
+                labelС.Visible = true;
+                labelEquals.Visible = true;
+                labelAnswer.Visible = true;
+
+                // Скрыть поля для ввода и вывода, если выбран другой RadioButton
+                // (предположим, что textBox3 и textBox4 - другие поля для ввода и вывода)
+                textBox3.Visible = false;
+                textBox4.Visible = false;
+                textBox5.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                label5.Visible = false;
+                labelA.Visible = false;
+                labelP.Visible = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            labelAnswer.Text = "?";
+            if (radioButton2.Checked)
+            {
+                // Показать поля для ввода и вывода, если выбран первый RadioButton
+                textBox3.Visible = true;
+                textBox4.Visible = true;
+                label3.Visible = true;
+                label4.Visible = true;
+                labelM.Visible = true;
+                labelN.Visible = true;
+                labelEquals.Visible = true;
+                labelAnswer.Visible = true;
+                labelA.Visible = true;
+
+                // Скрыть поля для ввода и вывода, если выбран другой RadioButton
+                // (предположим, что textBox3 и textBox4 - другие поля для ввода и вывода)
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                textBox5.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                label5.Visible = false;
+                labelС.Visible = false;
+                labelP.Visible = false;
+
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            labelAnswer.Text = "?";
+            if (radioButton3.Checked)
+            {
+                // Показать поля для ввода и вывода, если выбран первый RadioButton
+                textBox5.Visible = true;
+                label5.Visible = true;
+                labelN.Visible = true;
+                labelEquals.Visible = true;
+                labelAnswer.Visible = true;
+                labelP.Visible = true;
+
+                // Скрыть поля для ввода и вывода, если выбран другой RadioButton
+                // (предположим, что textBox3 и textBox4 - другие поля для ввода и вывода)
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+                textBox4.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                labelM.Visible = false;
+                labelС.Visible = false;
+                labelA.Visible = false;
+            }
+        }
+
+        private void Solution_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                // Расчет C при выборе первого RadioButton
+                if (int.TryParse(textBox1.Text, out int n) && int.TryParse(textBox2.Text, out int m))
+                {
+                    double result = CalculateC(n, m);
+                    labelAnswer.Text = $"{result}";
+                }
+                else
+                {
+                    MessageBox.Show("Решение невозможно, неверно введены данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                ClearFields();
+            }
+            else if (radioButton2.Checked)
+            {
+                // Расчет A при выборе второго RadioButton
+                if (int.TryParse(textBox3.Text, out int n) && int.TryParse(textBox4.Text, out int m))
+                {
+                    double result = CalculateA(n, m);
+                    labelAnswer.Text = $"{result}";
+                }
+                else
+                {
+                    MessageBox.Show("Решение невозможно, неверно введены данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                ClearFields();
+            }
+            else if (radioButton3.Checked)
+            {
+                if (int.TryParse(textBox5.Text, out int n))
+                {
+                    double result = CalculateP(n);
+                    labelAnswer.Text = $"{result}";
+                }
+                else
+                {
+                    MessageBox.Show("Решение невозможно, неверно введены данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                ClearFields();
+            }
+        }
+        // Метод для вычисления C
+        private double CalculateC(int n, int m)
+        {
+            return Factorial(n) / (Factorial(m) * Factorial(n - m));
+        }
+
+        // Метод для вычисления A
+        private double CalculateA(int n, int m)
+        {
+            return Factorial(n) / Factorial(n - m);
+        }
+
+        // Метод для вычисления факториала числа
+
+        private double Factorial(int num)
+        {
+            double factorial = 1;
+            for (int i = 2; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+        // Метод для вычисления P
+        private double CalculateP(int num)
+        {
+            double factorial = 1;
+            for (int i = 2; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;   
+        }
+        private void ClearFields()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox5.Text, out int number))
+            {
+                textBox5.Text = "";
+            }
+            else if (number > 10 || number < 0)
+            {
+                MessageBox.Show("Число должно быть от 0 до 10.");
+                textBox5.Text = "";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int number))
+            {
+                textBox1.Text = "";
+            }
+            else if (number > 10 || number < 0)
+            {
+                MessageBox.Show("Число должно быть от 0 до 10.");
+                textBox1.Text = "";
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox4.Text, out int number))
+            {
+                textBox4.Text = "";
+            }
+            else if (number > 10 || number < 0)
+            {
+                MessageBox.Show("Число должно быть от 0 до 10.");
+                textBox4.Text = "";
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox3.Text, out int number))
+            {
+                textBox3.Text = "";
+            }
+            else if (number > 10 || number < 0)
+            {
+                MessageBox.Show("Число должно быть от 0 до 10.");
+                textBox3.Text = "";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox2.Text, out int number))
+            {
+                textBox2.Text = "";
+            }
+            else if (number > 10 || number < 0)
+            {
+                MessageBox.Show("Число должно быть от 0 до 10.");
+                textBox2.Text = "";
+            }
+        }
     }
 }
