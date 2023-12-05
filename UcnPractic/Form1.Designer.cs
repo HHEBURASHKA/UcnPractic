d5d261bf2593eb0ba1ca197318298a61ecebcddf
HHEBURASHKA <an_drei_artamonov@mail.ru>
2023-12-05 17:51:58 +0500

    Доделать (возможно)

--- UcnPractic/Form1.Designer.cs
+++ UcnPractic/Form1.Designer.cs
@@ -28,13 +28,264 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Calculation = new System.Windows.Forms.Button();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxStep = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxXmax = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxXmin = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(670, 389);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Calculation
+            // 
+            this.Calculation.Location = new System.Drawing.Point(380, 422);
+            this.Calculation.Name = "Calculation";
+            this.Calculation.Size = new System.Drawing.Size(75, 23);
+            this.Calculation.TabIndex = 1;
+            this.Calculation.Text = "Расчет";
+            this.Calculation.UseVisualStyleBackColor = true;
+            this.Calculation.Click += new System.EventHandler(this.Calculation_Click);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(12, 421);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(35, 20);
+            this.textBoxA.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 405);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "b";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(53, 421);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(35, 20);
+            this.textBoxB.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 406);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "c";
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(94, 422);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(35, 20);
+            this.textBoxC.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(135, 406);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "x";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(135, 422);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(35, 20);
+            this.textBoxX.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(176, 406);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(12, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "y";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(176, 422);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(35, 20);
+            this.textBoxY.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(339, 407);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Step";
+            // 
+            // textBoxStep
+            // 
+            this.textBoxStep.Location = new System.Drawing.Point(339, 423);
+            this.textBoxStep.Name = "textBoxStep";
+            this.textBoxStep.Size = new System.Drawing.Size(35, 20);
+            this.textBoxStep.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(298, 407);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Xmax";
+            // 
+            // textBoxXmax
+            // 
+            this.textBoxXmax.Location = new System.Drawing.Point(298, 423);
+            this.textBoxXmax.Name = "textBoxXmax";
+            this.textBoxXmax.Size = new System.Drawing.Size(35, 20);
+            this.textBoxXmax.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(257, 406);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Xmin";
+            // 
+            // textBoxXmin
+            // 
+            this.textBoxXmin.Location = new System.Drawing.Point(257, 422);
+            this.textBoxXmin.Name = "textBoxXmin";
+            this.textBoxXmin.Size = new System.Drawing.Size(35, 20);
+            this.textBoxXmin.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(216, 406);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(12, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "z";
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Location = new System.Drawing.Point(216, 422);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(35, 20);
+            this.textBoxZ.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(670, 454);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxStep);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxXmax);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxXmin);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.Calculation);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Schedule";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button Calculation;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxStep;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxXmax;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxXmin;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxZ;
     }
 }
 
--- UcnPractic/Form1.cs
+++ UcnPractic/Form1.cs
@@ -16,5 +16,43 @@ namespace UcnPractic
         {
             InitializeComponent();
         }
+
+        private void Calculation_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBoxA.Text);
+            double b = double.Parse(textBoxB.Text);
+            double c = double.Parse(textBoxC.Text);
+            double x = double.Parse(textBoxX.Text);
+            double y = double.Parse(textBoxY.Text);
+            double z = double.Parse(textBoxZ.Text);
+            double Xmin = double.Parse(textBoxXmin.Text);
+            double Xmax = double.Parse(textBoxXmax.Text);
+            double Step = double.Parse(textBoxStep.Text);
+            //количество точек графика
+            int count = (int)Math.Ceiling((Xmax - Xmin) / Step)
+                + 1;
+            //массив значений X - общий для обоих графиков
+            double[] X = new double[count];
+            double[] t1 = new double[count];
+            double[] t2 = new double[count];
+            //расчитываем точки для графиков функции
+            for (int i = 0; i < count; i++)
+            {
+                //вычисляем значение
+                X[i] = Xmin + Step * i;
+                //вычисляем значение функций в точке X
+                t1[i] = (a * Math.Pow(x, 2)) + (b * x) + c;
+                t2[i] = (2 * Math.Cos(x - (Math.PI / 6))) / (0.5 + Math.Pow(Math.Sin(y), 2)) * (1 + (Math.Pow(z, 2) / ((3 - Math.Pow(z, 2)) / 5)));
+            }
+            //настраиваем оси графика
+            chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+            //определяем шаг сетки 
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step;
+
+            //добавляем вычисление значения в графики
+            chart1.Series[0].Points.DataBindXY(X, t1);
+            chart1.Series[1].Points.DataBindXY(X, t2);
+        }
     }
 }
